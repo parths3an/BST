bb46a0be6d6f0a88984508c23c9740051ead327c
Parth M Shah <pms002@ieng6-202.ucsd.edu>
2017-11-20 11:50:03 -0800

    Initial and require files

---
+++ node.h
@@ -0,0 +1,22 @@
+/**
+ * declaration of the node class
+ */
+//TODO: Create a template based Node class
+#ifndef NODE_H
+#define NODE_H
+
+#include<iostream>
+
+using namespace std;
+ class Node 
+ {
+ public: 
+    //constructor
+    Node(int newData):data(newData),right(nullptr), left(nullptr), parent(nullptr){}
+
+    int data;
+    Node* right;
+    Node* left;
+    Node* parent;
+ };
+#endif
