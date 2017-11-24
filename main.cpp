04e3b517fdfc34a9d510907ee39e3b1f2cb75eb8
Parth M Shah <pms002@ieng6-201.ucsd.edu>
2017-11-24 02:09:20 -0800

    Tested all 3 DFS; they work fine.

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main()
 cout << "Created a new BST Object... \n";
 BST b1;
 cout << "Testing empty size... size should be zero and it is: " << b1.getSize() << endl;
-cout << "Testing insert... Inserting 25...\n";
+cout << "\nTesting insert... Inserting 25...\n";
 b1.insert(25);
 cout << "Root of the tree should be 25 and it is...: " << b1.getRootData() << endl;
 
@@ -22,6 +22,19 @@ b1.insert(20);
 b1.insert(21);
 b1.insert(28);
 b1.insert(30);
+
+cout << "\n\nTesting the DFS printing algos: \n \n";
+cout << "Testing inOrder traversal: It should be:  20 21 24 25 28 30" << endl;
+b1.printInOrder();
+cout << endl;
+
+cout << "\nTesting preOrder traversal: It should be: 25 24 20 21 28 30\n" << endl;
+b1.printPreOrder();
+cout << endl;
+
+cout << "\nTesting postOrder traversal: It should be:  21 20 24 30 28 25\n" << endl;
+b1.printPostOrder();
+cout << endl;
 
 return 0;
 }
